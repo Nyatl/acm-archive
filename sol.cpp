083d1bc38dde2749d51555b3925a9f17be74b4a1
Nyatl <nyatl@yandex.ru>
2018-02-10 23:37:41 +0400

    improved sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -19,6 +19,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <cstdarg>
 #include <memory.h>
 #include <assert.h>
 
@@ -45,6 +46,34 @@ typedef pair<int, int> pii;
 typedef vector<pii> vpii;
 typedef set<int> si;
 typedef set<pii> spii;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef set<ll> sl;
+typedef set<pll> spll;
+
+char __buffer[100000];
+
+void err(const char *format, ... ) {
+#ifdef LOCAL
+	va_list ap;
+	va_start(ap, format);
+	vsprintf(__buffer, format, ap);
+	va_end(ap);
+	
+	fprintf(stderr, "%s\n", __buffer);
+#else
+	if (format < 0) {
+		__buffer[0]++;
+	}
+#endif
+}
+
+void exit() {
+	err("TIME: %lf\n", (double)clock() / CLOCKS_PER_SEC);
+	exit(0);
+}
 
 template<typename A, typename B>
 ostream& operator<<(ostream& s, pair<A, B> p) {
@@ -68,6 +97,15 @@ ostream& operator<<(ostream& s, set<T> t) {
 	return s;
 }
 
+template<typename T1, typename T2>
+ostream& operator<<(ostream& s, map<T1, T2> t) {
+	cerr << endl;
+	for (auto z : t) {
+		cerr << "\t" << z.first << " -> " << z.second << endl;
+	}
+	return s;
+}
+
 #ifdef LOCAL
 #define dbg(x) cerr << __LINE__ << "\t" << #x << ": " << x << endl;
 #define dbg0(x, n) cerr << __LINE__ << "\t" << #x << ": "; for (int ABC = 0; ABC < n; ABC++) cerr << x[ABC] << ' '; cerr << endl;
@@ -85,5 +123,14 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
+
+	int a = 0;
+	ll b = a;
+	int c = b;
+	int d = 0;
+	
+	dbg(c);
+	
+	exit();
 	return 0;
 }
