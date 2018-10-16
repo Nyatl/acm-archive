e4a9b9dc535ded5491ee5483743a47196e5b602d
Pavel Abizyaev <pavel.abizyaev@apicasystem.com>
2018-10-16 15:59:22 +0400

    Update topcoder.cpp

--- topcoder.cpp
+++ topcoder.cpp
@@ -14,6 +14,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
 #include <cmath>
 #include <cstdarg>
 #include <cassert>
@@ -21,7 +22,6 @@
 #include <tuple>
 #include <unordered_set>
 #include <unordered_map>
-#include <memory.h>
 
 using namespace std;
 
