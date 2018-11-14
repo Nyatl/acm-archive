7c3d537653b6cf8722ebeb4b3a96f135fe51ae3b
Filyan <filyanvulcan@gmail.com>
2018-11-14 16:58:21 +0400

    templates: gauss

---
+++ templates/math/gauss.cpp
@@ -0,0 +1,44 @@
+int a[MAX][MAX];
+int n, m;
+int ans[MAX];
+int who[MAX];
+
+bool gauss() {
+	fill_n(&who[1], n, -1);
+	int r = 1;
+	for (int col = 1; col <= m; col++) {
+		int p = -1;
+		fi(r, n) {
+			if (a[i][col]) {
+				p = i;
+				break;
+			}
+		}
+		if (p == -1) {
+			continue;
+		}
+		who[r] = col;
+		fi(1, m + 1) {
+			swap(a[r][i], a[p][i]);
+		}
+		fi(1, n) {
+			if (i == r) continue;
+			int c = dev(a[i][col], a[r][col]);
+			fj(1, m + 1) {
+				a[i][j] = sub(a[i][j], mul(a[r][j], c));
+			}
+		}
+		r++;
+	}
+	fill_n(&ans[1], m, 0);
+	fi(1, n) {
+		if (!a[i][m + 1]) continue;
+		if (who[i] == -1) return false;
+		int c = dev(a[i][m + 1], a[i][who[i]]);
+		ans[who[i]] = c;
+	}
+	return true;
+}
+
+
+
