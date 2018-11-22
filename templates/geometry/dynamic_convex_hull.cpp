a7bd1ace81ffc0c7e5885e5ef77d75fd72fea9d6
Filyan <filyanvulcan@gmail.com>
2018-11-22 13:44:15 +0400

    templates: dynamic_convex_hull(COPYPASTED!)

---
+++ templates/geometry/dynamic_convex_hull.cpp
@@ -0,0 +1,43 @@
+//tested: https://bacs.cs.istu.ru/submit_view.php?id=1382619
+
+//code credit: https://github.com/niklasb/contest-algos/blob/master/convex_hull/dynamic.cpp
+typedef ll line_t;
+
+const line_t is_query = -(1LL << 60);
+
+struct Line {
+    line_t m, b;
+    mutable function<const Line*()> succ;
+    bool operator<(const Line& rhs) const {
+        if (rhs.b != is_query) return m < rhs.m;
+        const Line* s = succ();
+        if (!s) return 0;
+        line_t x = rhs.m;
+        return b - s->b < (s->m - m) * x;
+    }
+};
+struct HullDynamic : public multiset<Line> { // will maintain upper hull for maximum
+    bool bad(iterator y) {
+        auto z = next(y);
+        if (y == begin()) {
+            if (z == end()) return 0;
+            return y->m == z->m && y->b <= z->b;
+        }
+        auto x = prev(y);
+        if (z == end()) return y->m == x->m && y->b <= x->b;
+        return (x->b - y->b)*(z->m - y->m) >= (y->b - z->b)*(y->m - x->m);
+    }
+    void insert_line(line_t m, line_t b) {
+        auto y = insert({ m, b });
+        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
+        if (bad(y)) { erase(y); return; }
+        while (next(y) != end() && bad(next(y))) erase(next(y));
+        while (y != begin() && bad(prev(y))) erase(prev(y));
+    }
+    line_t eval(line_t x) {
+        auto l = *lower_bound((Line) { x, is_query });
+        return l.m * x + l.b;
+    }
+};
+
+//end code credit
